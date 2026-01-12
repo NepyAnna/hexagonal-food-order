1c10607d7ee6dc96355f4a2858687fac51a33bb8
NepyAnna <nepyann@gmail.com>
2026-01-12 17:45:33 +0100

    initial commit

---
+++ src/main/java/com/sheoanna/hexagonal_food_order/HexagonalFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.sheoanna.hexagonall_foodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonallFoodorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonallFoodorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheoanna/hexagonal_food_order/HexagonalFoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheoanna.hexagonall_foodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonallFoodorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
