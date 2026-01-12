c971a0a3aec655436aa2b609f86cedb27898446a
NepyAnna <nepyann@gmail.com>
2026-01-12 17:58:43 +0100

    feat: add OrderEntity

---
+++ src/main/java/com/sheoanna/hexagonal_food_order/adapter/input/rest/OrderController.java
@@ -0,0 +1,4 @@
+package com.sheoanna.hexagonal_food_order.adapter.input.rest;
+
+public class OrderController {
+}
---
+++ src/main/java/com/sheoanna/hexagonal_food_order/adapter/output/entity/OrderEntity.java
@@ -0,0 +1,18 @@
+package com.sheoanna.hexagonal_food_order.adapter.output.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class OrderEntity {
+    @Id
+    private String orderId;
+    private String customerName;
+    private String restaurantName;
+    private String item;
+    private String status;
+}
